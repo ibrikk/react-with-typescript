f011d68ae2a071d6e79363c0591967a74a7f7aea
Ibrahim Khalilov <ibrahimkhalilov@Ibrahims-Mac-mini.local>
2021-05-18 01:47:17 -0400

    how to add props, useReducer, and useState in TS

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import {TextField} from './components/TextField';
+import {Counter} from './components/Counter';
 
 //props
 //hooks
@@ -9,7 +10,18 @@ import {TextField} from './components/TextField';
 const App: React.FC = () => {
   return (
     <div>
-      <TextField text='hello' person={{firstName: '', lastName: '' }}/>
+      <TextField text='hello' person={{firstName: '', lastName: '' }}
+      
+      />
+      <Counter>
+        {({count, setCount}) => (<div>
+          {count}
+          <button onClick={() => setCount(count + 1)}>
+            +
+          </button>
+
+        </div>)}
+      </Counter>
     </div>
   )
 }
